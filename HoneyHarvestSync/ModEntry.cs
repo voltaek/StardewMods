d63e7be0c8cebe50718a96e92f04fd8a99a6d58e
voltaek <7809420+voltaek@users.noreply.github.com>
2024-03-10 01:02:58 -0500

    Initial commit of HoneyHarvestSync v1.0.0. Tested and working for Stardew Valley v1.5.6 with SMAPI v3.18.6, with no SMAPI v4.0 deprecation warnings. Have not tested with the Stardew Valley v1.6 modding alpha, yet.

---
+++ HoneyHarvestSync/ModEntry.cs
@@ -0,0 +1,27 @@
+﻿using StardewModdingAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoneyHarvestSync
+{
+	internal sealed class ModEntry : Mod
+	{
+		/// <summary>The mod entry point, called after the mod is first loaded.</summary>
+		/// <param name="helper">Provides simplified APIs for writing mods.</param>
+		public override void Entry(IModHelper helper)
+		{
+			// Hold onto the monitor so we can do logging
+			HoneyUpdater.Monitor = Monitor;
+
+			// Rig up the event handlers we need to do proper tracking of bee houses and flowers
+			Helper.Events.GameLoop.DayStarted += HoneyUpdater.OnDayStarted;
+			Helper.Events.GameLoop.TimeChanged += HoneyUpdater.OnTimeChanged;
+			Helper.Events.GameLoop.OneSecondUpdateTicked += HoneyUpdater.OnOneSecondUpdateTicked;
+			Helper.Events.World.ObjectListChanged += HoneyUpdater.OnObjectListChanged;
+			Helper.Events.World.LocationListChanged += HoneyUpdater.OnLocationListChanged;
+		}
+	}
+}
